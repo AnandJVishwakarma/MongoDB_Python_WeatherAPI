9e5ea241b73a68770eee5feeaa5b6af51e0e543a
Anand Vishwakarma <55125207+AnandJVishwakarma@users.noreply.github.com>
2023-12-01 17:11:57 +0530

    Update weather_data_collector.py

--- weather_data_collector.py
+++ weather_data_collector.py
@@ -1,30 +1,14 @@
-import sys
-import os
 import requests
 import json
+from config import WEATHER_API_GET_DATA, WEATHER_API_GET_LAT_LONG, WEATHER_APIKEY
+from common_functions import printException
 from mongodb_crud import insertData
 
-# =========================================== VARIBALES : START ================================================== #
-# Secret keys imports...
-WEATHER_APIKEY = os.getenv('OPEN_WEATHER_API_KEY','11becd9b4f1c073ab5ea578f43d33f3a')
-# External URLs...
-WEATHER_API_GET_DATA     = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&exclude={part}&appid={APIkey}"
-WEATHER_API_GET_LAT_LONG = "http://api.openweathermap.org/geo/1.0/direct?q={location}&appid={APIkey}"
-# =========================================== VARIBALES : END ================================================== #
-
-
-# =========================================== COMMON FUNCTIONS : START ================================================== #
-def printException(func_name: str, error: str) -> None:
-    print(f'{func_name}: {error}')
-    _, __, exc_tb = sys.exc_info()
-    print(f'Line No: {exc_tb.tb_lineno}')
-# =========================================== COMMON FUNCTIONS : END ================================================== #
-
-
+# Description: Fetch latitude and longitude based on location name from openweatherapi.
 def getlatlongByLoctnName(location_name: str) -> dict:
     try:
         if location_name:
-            get_latlong = WEATHER_API_GET_LAT_LONG.format(location = location_name,APIkey = WEATHER_APIKEY)
+            get_latlong = WEATHER_API_GET_LAT_LONG.format(location = location_name, APIkey = WEATHER_APIKEY)
             get_latlong_response = requests.get(url=get_latlong)
             get_latlong_response = json.loads(get_latlong_response.content)
             lat, long = get_latlong_response[0]['lat'], get_latlong_response[0]['lon']
@@ -32,6 +16,7 @@ def getlatlongByLoctnName(location_name: str) -> dict:
     except (Exception) as error:
         printException("getlatlongByLoctnName", error)
 
+# Description: Fetch wether data for particular location from openweatherapi.
 def getWeatherData(request: dict) -> dict:
     try:
         if 'location' in request and request['location']:
@@ -39,16 +24,16 @@ def getWeatherData(request: dict) -> dict:
             latlong_data = getlatlongByLoctnName(request['location'])
             if 'lat' in latlong_data and 'long' in latlong_data:
                 lat, long =  latlong_data['lat'], latlong_data['long']
-            get_weather_data = WEATHER_API_GET_DATA.format(lat = lat, lon = long, part = "daily",APIkey = WEATHER_APIKEY)
+            get_weather_data = WEATHER_API_GET_DATA.format(lat = lat, lon = long, part = "daily", APIkey = WEATHER_APIKEY)
             get_weather_dataresponse = requests.get(url=get_weather_data)
             get_weather_dataresponse = json.loads(get_weather_dataresponse.content)
             return {'lat':lat,'long': long, "data":get_weather_dataresponse}
     except (Exception) as error:
         printException("getWeatherData", error)
 
-
+# Description: Main function.
 def main():
-    user_input_location = str(input("ENter the location name: "))
+    user_input_location = str(input("Enter the location name: "))
     weather_service = getWeatherData({
         "location":user_input_location
     })
